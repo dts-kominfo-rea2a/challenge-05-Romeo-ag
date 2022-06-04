cdb01eeefb4b7ba4998743496262e0691eb6d59e
Romeo-ag <romeo.a.gurning@gmail.com>
2022-06-04 11:43:50 -0400

    menambahkan definisi sorter dengan callback ascending dan descending

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (inArray,fnCallback)=>{
+  // console.log(inArray);
+  // console.log(inArray.sort());
+  // console.log(inArray.reverse());
+  let result = [];
+  let sortedArray = fnCallback(inArray);
+  // console.log(sortedArray)
+  for(i =0; i<sortedArray.length; i++){
+    result.push(i+1+ ". " + sortedArray[i]);
+  }
+  return result;
+
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (inArray)=>{
+  // console.log(inArray.sort());
+  return inArray.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (inArray)=>{
+  return inArray.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
